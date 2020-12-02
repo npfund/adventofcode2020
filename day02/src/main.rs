96b3358920ff1af1335c2acfe37eb69e884fc6d6
Nick Pfundstein <nickpfundstein@gmail.com>
2020-12-02 08:20:21 -0700

    Day 2 Part 2 complete

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -28,5 +28,24 @@ fn part1() {
 }
 
 fn part2() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
 
+    let mut valid = 0;
+    let pattern = Regex::new("(?P<first>[0-9]+)-(?P<second>[0-9]+) (?P<char>[a-z]): (?P<password>[a-z]+)").unwrap();
+    for line in file.lines().map(|x| x.unwrap()) {
+        let captures = pattern.captures(&line).unwrap();
+        let first = captures.name("first").unwrap().as_str().parse::<usize>().unwrap() - 1;
+        let second = captures.name("second").unwrap().as_str().parse::<usize>().unwrap() - 1;
+        let char = captures.name("char").unwrap().as_str().chars().nth(0).unwrap();
+        let password = captures.name("password").unwrap().as_str();
+        if let Some(first_char) = password.chars().nth(first) {
+            if let Some(second_char) = password.chars().nth(second) {
+                if (first_char == char) ^ (second_char == char) {
+                    valid += 1;
+                }
+            }
+        }
+    }
+
+    println!("{} valid passwords", valid);
 }
