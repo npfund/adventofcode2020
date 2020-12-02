1f4d388de7ad6d912fd1b40f6861dbbfb05e7044
Nick Pfundstein <nickpfundstein@gmail.com>
2020-12-02 08:06:22 -0700

    Day 2 Part 1 complete

---
+++ day02/src/main.rs
@@ -0,0 +1,32 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use regex::Regex;
+
+fn main() {
+    part1();
+    part2();
+}
+
+fn part1() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    let mut valid = 0;
+    let pattern = Regex::new("(?P<min>[0-9]+)-(?P<max>[0-9]+) (?P<char>[a-z]): (?P<password>[a-z]+)").unwrap();
+    for line in file.lines().map(|x| x.unwrap()) {
+        let captures = pattern.captures(&line).unwrap();
+        let min = captures.name("min").unwrap().as_str().parse::<usize>().unwrap();
+        let max = captures.name("max").unwrap().as_str().parse::<usize>().unwrap();
+        let char = captures.name("char").unwrap().as_str().chars().nth(0).unwrap();
+        let password = captures.name("password").unwrap().as_str();
+        let matches = password.chars().filter(|&c| c == char).count();
+        if min <= matches && matches <= max {
+            valid += 1;
+        }
+    }
+
+    println!("{} valid passwords", valid);
+}
+
+fn part2() {
+
+}
