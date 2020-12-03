181f587f3fd17d66772acd657a4b6089f8f9ec7d
Nick Pfundstein <nickpfundstein@gmail.com>
2020-12-03 08:10:24 -0700

    Day 3 Part 2 complete

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -25,4 +25,30 @@ fn part1() {
 }
 
 fn part2() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    let forest = file.lines().map(|x| x.unwrap()).collect::<Vec<_>>();
+
+    let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+
+    let mut total: u64 = 1;
+    for (dx, dy) in slopes.iter() {
+        let mut x = 0;
+        let mut y = 0;
+        let mut trees = 0;
+        while y < forest.len() {
+            let row = &forest[y];
+            if row.chars().nth(x % row.len()).unwrap() == '#' {
+                trees += 1;
+            }
+
+            x += dx;
+            y += dy;
+        }
+
+        println!("{}/{} {}", dx, dy, trees);
+        total *= trees;
+    }
+
+    println!("{}", total);
 }
