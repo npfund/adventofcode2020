8a9edc35a7df973f570412079fa50bfbd0ede300
Nick Pfundstein <nickpfundstein@gmail.com>
2020-12-04 09:12:24 -0700

    Day 2 Part 2 complete

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs;
-use std::io::{BufRead, BufReader};
+use regex::Regex;
 
 fn main() {
     part1();
@@ -28,4 +28,63 @@ fn part1() {
 }
 
 fn part2() {
+    let file = fs::read_to_string("input.txt").unwrap();
+
+    let byr_regex = Regex::new("byr:([0-9]{4})( |$)").unwrap();
+    let iyr_regex = Regex::new("iyr:([0-9]{4})( |$)").unwrap();
+    let eyr_regex = Regex::new("eyr:([0-9]{4})( |$)").unwrap();
+    let hgt_regex = Regex::new("hgt:([0-9]+)(cm|in)( |$)").unwrap();
+    let hcl_regex = Regex::new("hcl:#[0-9a-f]{6}( |$)").unwrap();
+    let ecl_regex = Regex::new("ecl:(amb|blu|brn|gry|grn|hzl|oth)( |$)").unwrap();
+    let pid_regex = Regex::new("pid:[0-9]{9}( |$)").unwrap();
+
+    let valid = file.split("\n\n")
+        .map(|p| p.replace("\n", " "))
+        .fold(0, |accum, passport| {
+            let byr_valid = if let Some(captures) = byr_regex.captures(&passport) {
+                let value = captures.get(1).unwrap().as_str().parse::<i32>().unwrap();
+                1920 <= value && value <= 2002
+            } else {
+                false
+            };
+
+            let iyr_valid = if let Some(captures) = iyr_regex.captures(&passport) {
+                let value = captures.get(1).unwrap().as_str().parse::<i32>().unwrap();
+                2010 <= value && value <= 2020
+            } else {
+                false
+            };
+
+            let eyr_valid = if let Some(captures) = eyr_regex.captures(&passport) {
+                let value = captures.get(1).unwrap().as_str().parse::<i32>().unwrap();
+                2020 <= value && value <= 2030
+            } else {
+                false
+            };
+
+            let hgt_valid = if let Some(captures) = hgt_regex.captures(&passport) {
+                let value = captures.get(1).unwrap().as_str().parse::<i32>().unwrap();
+                let unit = captures.get(2).unwrap().as_str();
+                if unit == "cm" {
+                    150 <= value && value <= 193
+                } else {
+                    59 <= value && value <= 76
+                }
+            } else {
+                false
+            };
+
+            let non_capturing = hcl_regex.is_match(&passport)
+                && ecl_regex.is_match(&passport)
+                && pid_regex.is_match(&passport);
+
+            if byr_valid && iyr_valid && eyr_valid && hgt_valid && non_capturing {
+                println!("{}\n", passport);
+                accum + 1
+            } else {
+                accum
+            }
+        });
+
+    println!("{}", valid);
 }
