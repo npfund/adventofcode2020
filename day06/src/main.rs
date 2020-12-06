d8904b9d4bd32ee263a5d32d6fbd9bba2f3e2b24
Nick Pfundstein <nickpfundstein@gmail.com>
2020-12-06 08:48:05 -0700

    Day 6 Part 2 complete

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 fn main() {
     part1();
@@ -24,4 +24,24 @@ fn part1() {
 }
 
 fn part2() {
+    let file = fs::read_to_string("input.txt").unwrap();
+
+    let total = file.split("\n\n")
+        .map(|group| {
+            let people = group.lines().count();
+            group.replace("\n", "")
+                .chars()
+                .fold(HashMap::new(), |mut accum, char| {
+                    let entry = accum.entry(char).or_insert(0_usize);
+                    *entry += 1;
+                    accum
+                })
+                .iter()
+                .filter(|(_k, v)| {
+                    **v == people
+                }).count()
+        })
+        .sum::<usize>();
+
+    println!("{}", total);
 }
