93060befdf5a415c8a6560436a4d2abb7a25507b
Nick Pfundstein <nickpfundstein@gmail.com>
2020-12-11 09:02:46 -0700

    Day 11 Part 2 complete

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -52,4 +52,56 @@ fn part1() {
 }
 
 fn part2() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    let mut seats = file.lines()
+        .map(|x| x.unwrap())
+        .enumerate()
+        .fold(HashMap::new(), |mut accum, (y, line)| {
+            for (x, char) in line.chars().enumerate() {
+                accum.insert((x as i32, y as i32), char);
+            }
+
+            accum
+        });
+
+    let deltas = [(0, 1), (1, 0), (1, 1), (0, -1), (-1, 0), (-1, -1), (1, -1), (-1, 1)];
+    loop {
+        let mut altered_seats = seats.clone();
+
+        for ((x, y), point) in seats.iter() {
+            if *point != '.' {
+                let neighbors = deltas.iter().fold(0, |mut accum, (dx, dy)| {
+                    let mut target = (x + dx, y + dy);
+                    while let Some(char) = seats.get(&target) {
+                        accum += match char {
+                            '#' => 1,
+                            _ => 0,
+                        };
+                        if *char == '#' || *char == 'L' {
+                            break;
+                        }
+                        target = (target.0 + dx, target.1 + dy);
+                    }
+
+                    accum
+                });
+
+                if *point == 'L' && neighbors == 0 {
+                    altered_seats.insert((*x, *y), '#');
+                } else if *point == '#' && neighbors >= 5 {
+                    altered_seats.insert((*x, *y), 'L');
+                }
+            }
+        }
+
+        let same = seats == altered_seats;
+        seats = altered_seats;
+        if same {
+            break;
+        }
+    }
+
+    let occupied = seats.values().filter(|x| **x == '#').count();
+    println!("{}", occupied);
 }
