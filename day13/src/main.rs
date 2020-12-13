be71da525a7be526a7e52b9447c7bc504a43dae9
Nick Pfundstein <nickpfundstein@gmail.com>
2020-12-13 09:13:04 -0700

    Day 13 Part 2 complete

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -32,4 +32,46 @@ fn part1() {
 }
 
 fn part2() {
+    let file = std::fs::read_to_string("input.txt").unwrap();
+
+    let ids = file.split("\n")
+        .skip(1)
+        .next()
+        .unwrap()
+        .split(",")
+        .enumerate()
+        .filter(|(_, s)| s != &"x")
+        .map(|(position, id)| (position as i64, id.parse().unwrap()))
+        .collect::<Vec<_>>();
+
+    let modulopods = ids.iter().map(|&(_, id)| id).collect::<Vec<_>>();
+    let diffs = ids.iter().map(|&(position, id)| id - position).collect::<Vec<_>>();
+
+    let product = modulopods.iter().product::<i64>();
+    let mut sum = 0;
+    for (&diff, modulus) in diffs.iter().zip(modulopods) {
+        let p = product / modulus;
+        sum += diff * mod_inv(p, modulus).unwrap() * p
+    }
+
+    println!("{}", sum % product);
+}
+
+// from: https://rosettacode.org/wiki/Chinese_remainder_theorem#Rust
+fn egcd(a: i64, b: i64) -> (i64, i64, i64) {
+    if a == 0 {
+        (b, 0, 1)
+    } else {
+        let (g, x, y) = egcd(b % a, a);
+        (g, y - (b / a) * x, x)
+    }
+}
+
+fn mod_inv(x: i64, n: i64) -> Option<i64> {
+    let (g, x, _) = egcd(x, n);
+    if g == 1 {
+        Some((x % n + n) % n)
+    } else {
+        None
+    }
 }
